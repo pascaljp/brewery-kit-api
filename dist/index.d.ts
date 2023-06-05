36bef704a5b6bff8027b107694e4f7eb4fe7e37b
Koichi Suematsu <pascal.jp@gmail.com>
2023-06-05 12:35:31 +0900

    Move to MQTT protocol from my own protocol.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,1 @@
-export * from './api';
-export { ClientApi } from './client';
-export { ServerApi } from './server';
 export { Logs } from '../proto/genfiles/api_pb';
--- dist/server.d.ts
+++
@@ -1,8 +0,0 @@
-import { DateTime } from 'luxon';
-import { Logs } from '../proto/genfiles/api_pb';
-export declare class ServerApi {
-    private baseDir_;
-    constructor(baseDir: string);
-    save(proto: Logs.ILogs): void;
-    load(type: keyof Logs.ILogs, date: DateTime): Logs.Logs;
-}
