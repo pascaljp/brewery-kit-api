45f44de67a29cd2de529f774d63a24db0c692d3a
Koichi Suematsu <pascal.jp@gmail.com>
2023-06-20 12:52:03 +0900

    Export the new API

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Api = void 0;
+exports.Api2 = exports.Api = void 0;
 var api_pb_1 = require("../proto/genfiles/api_pb");
 Object.defineProperty(exports, "Api", { enumerable: true, get: function () { return api_pb_1.Api; } });
+var api2_pb_1 = require("../proto/genfiles/api2_pb");
+Object.defineProperty(exports, "Api2", { enumerable: true, get: function () { return api2_pb_1.Api2; } });
--- ts/index.ts
+++ ts/index.ts
@@ -1,1 +1,2 @@
 export {Api} from '../proto/genfiles/api_pb';
+export {Api2} from '../proto/genfiles/api2_pb';
